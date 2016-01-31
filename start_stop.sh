f3f49a4b9855d4bc17c1d1802b60a91fdae76e9e
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-01-31 14:12:35 +0100

    updates

--- start_stop.sh
+++ start_stop.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# Startables and stoppables
+# startables and stoppables
 
-# postgres
-alias start-pgsql=""
-alias stop-pgsql=""
+# postgres (currently running automatically in background)
+#alias start-pg=""
+#alias stop-pg=""
 
 # mysql (http://stackoverflow.com/questions/11091414/how-to-stop-mysqld)
-alias start-mysql=""
-alias stop-mysql="mysqladmin -u root -p shutdown"
+alias start-mysql="mysql.server start"
+alias stop-mysql="mysqladmin -u root shutdown"
 
 # neo4j
 alias start-neo4j="neo4j start"
 alias stop-neo4k="neo4j stop"
 
-
 # find
-alias find-stoppables="ps aux | grep ..."
+alias find-stoppables="ps aux | egrep 'sql|neo4j' --color"
