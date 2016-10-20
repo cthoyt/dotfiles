b472420b6b81743867be2a64818cf655cdc2fb23
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-10-20 23:13:55 +0200

    fix get secrets... again

--- get_secrets.sh
+++ get_secrets.sh
@@ -11,4 +11,4 @@ cp ~/.shuttle.json $SECRETS/shuttle.json
 cp -r ~/.config $SECRETS/config/
 cp -r ~/.ssh $SECRETS/ssh/
 cp ~/.pypirc $SECRETS/pypirc.ini
-cp ~/.pip/pip.conf
+cp ~/.pip/pip.conf $SECRETS/pip.conf
